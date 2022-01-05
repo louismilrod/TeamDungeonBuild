aa176f909836d43d17b83c4c36b8d8c3e9b8bab9
Louis Milrod <int4rweb@gmail.com>
2022-01-05 16:13:24 -0500

    push

--- ProgramUI/Program.cs
+++ ProgramUI/Program.cs
@@ -7,21 +7,20 @@ using System.IO;
 using static System.Console;
 using System.Threading;
 
+
 namespace ProgramUI
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            
-            
-        }
-        public void Run()
-        {
-            File.WriteAllLines("@www.github.com/therepo");
-            string text = File.ReadAllText(@);           
-            
-        }
-        
+
+            {
+                ProgramUI ui = new ProgramUI();
+                ui.Run();
+            }
+
+
+        } 
     }
 }
--- ProgramUI/ProgramUI.cs
+++ ProgramUI/ProgramUI.cs
@@ -11,28 +11,67 @@ namespace ProgramUI
 {
     public class ProgramUI
     {
+        public void Run()
+        {                
+            Seed();
+            ShowMenu();
+        }
 
-        static void Main(string[] args)
+        private void Seed()
         {
-            string[] HP = new string[]
+            File.WriteAllLines("@www.github.com/therepo");
+            string text = File.ReadAllText(@);
+            throw new NotImplementedException();
+        }
+
+        private void ShowMenu()
+        {
+            bool isRunning = true;
+            while (isRunning)
             {
-                "HP = 100"
+                Clear();
+                WriteLine("Welcome to your fantastic concert adventure your goal is to get in the door \n" +
+                    "1. Start a new run\n" +
+                    "2. Load a game \n" + 
+                    "3. Delete Game\n" + 
+                    "4. Exit \n");
                 
-            }
-            public bool HPCheck()
-            {
-                if (HP != 0)
+                string userinput = Console.ReadLine();
+                switch (userinput)
                 {
-                    return true;
+                    case "1":
+                        StartANewGame();
+                        break;
+                    case "2":
+                        LoadAGame();
+                        break;
+                    case "3":
+                        DeleteGame();
+                        break;
+                    case "4":
+                        isRunning = false;
+                        break;
+                    default: WriteLine("Please enter a valid selection");
+                        Thread.Sleep(500);
+                        break;
                 }
-                else (HP <= 0);
-                {
-                    Clear();
-                    WriteLine("You have been shut out and drove 8 hours for nothing");
-                    Thread.Sleep(1500);
-                    return false;
-                }
+
             }
+        }
+
+        private void StartANewGame()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void LoadAGame()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void DeleteGame()
+        {
+            throw new NotImplementedException();
         }
     }
 }
