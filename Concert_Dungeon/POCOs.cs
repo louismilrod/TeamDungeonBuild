f0d6a5ceb28c0b5e424b8c2a2f1b269673677b0e
Louis Milrod <int4rweb@gmail.com>
2022-01-05 16:45:59 -0500

    uploading maybe

--- Concert_Dungeon/POCOs.cs
+++ Concert_Dungeon/POCOs.cs
@@ -3,13 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace Concert_Dungeon
 {
     public class POCOs
     {
+        public int HitPoints { get; set; }
+        public bool HasTicket { get; set; }
+        public bool HasLadder { get; set; }
+        public bool HasVest { get; set; }
+        public bool Clipboard { get; set; }
 
-         
+        public int ScalpedTicket
+        {
+            set
+            {
+                Random ticket = new Random();
+                int x = ticket.Next(0, 50);
+                if (x > 25)
+                {
+                    Console.WriteLine("Welcome to the concert");
+                }
+                else
+                {
+                    Console.WriteLine("This ticket is fradulent and you were shamed by security");
+                    Thread.Sleep(3000);
+                }
+            }
+
+
+        }
+
 
     }
 }
--- ProgramUI/ProgramUI.cs
+++ ProgramUI/ProgramUI.cs
@@ -6,11 +6,13 @@ using System.Threading.Tasks;
 using System.Threading;
 using System.IO;
 using static System.Console;
+using static Concert_Dungeon.POCOs;
 
 namespace ProgramUI
 {
     public class ProgramUI
     {
+                    
         public void Run()
         {                
             Seed();
@@ -60,6 +62,99 @@ namespace ProgramUI
         }
 
         private void StartANewGame()
+        {
+            Clear();
+            Write("Welcome my son to the machine followed by an explanation of goals");
+            WriteLine("Choose which direction to go: \n" +
+                "1. Parking Lot A \n" +
+                "2. Parking Lot B \n" +
+                "3. Parking Lot C \n" +
+                "4. North Entrance \n" +
+                "5. South Entrance \n" +
+                "6. East Entrance \n" +
+                "7. Bathroom \n" +
+                "8. Box Office \n");
+
+            string userinput = ReadLine();
+
+            switch (userinput)
+            {
+                case "1":
+                    ParkingLotA();
+                    break;
+                case "2":
+                    ParkingLotB(); 
+                    break;
+                case "3":
+                    ParkingLotC();
+                    break;
+                case "4":
+                    NorthEnterance();
+                    break;
+                case "5":
+                    SouthEnterance();
+                    break;
+                case "6":
+                    EastEnterance();
+                    break;
+                case "7":
+                    Bathroom();
+                    break;
+                case "8":
+                    BoxOffice();
+                    break;
+                default:
+                    WriteLine("Please enter a valid selection");
+                    Thread.Sleep(500);
+                    break;
+            }
+        }
+
+        public bool BoxOffice()
+        {
+
+            if (hasTicket==true)
+            {
+                WriteLine("Enjoy the concert");
+            }
+            else
+            {
+                //offer to sell ticket if 
+            }
+
+        }
+
+        private void Bathroom()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void EastEnterance()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void SouthEnterance()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void NorthEnterance()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ParkingLotC()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ParkingLotB()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ParkingLotA()
         {
             throw new NotImplementedException();
         }
